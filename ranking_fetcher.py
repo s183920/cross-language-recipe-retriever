7b3e018a2882076d01e760cc07d01ba65c899203
Lukas Leindals <s183920@student.dtu.dk>
2023-11-22 11:18:10 -0500

    create ranking file function

--- ranking_fetcher.py
+++ ranking_fetcher.py
@@ -1,4 +1,7 @@
 import json
+import glob
+import re
+import os
 
 def fetch_rankings():
 
@@ -14,8 +17,46 @@ def fetch_rankings():
 
     return data
     
+    
+def create_ranking_file(language):
+    # get files
+    rankings_file = f"rankings/{language}.json"
+    metadata_files = glob.glob(f"google_results/*/{language}/*metadata.json")
+    # print(metadata)
+    
+    # check if rankings file exists
+    if os.path.exists(rankings_file):
+        with open(rankings_file, 'r') as file:
+            rankings = json.load(file)
+    else:
+        rankings = {}
+    
+    # 
+    for metadata_file in metadata_files:
+        # get query key
+        metadata_file = metadata_file.replace("\\", "/")
+        query_key = re.search(fr"google_results/(.*)/{language}.*", metadata_file).group(1)
+        
+        # check if query key is already in rankings - if so, skip to avoid overwriting
+        if query_key in rankings:
+            continue
+        
+        # load metadata
+        with open(metadata_file, 'r') as file:
+            metadata = json.load(file)
+            
+        # add ranking to dict
+        for entry in metadata:
+            metadata[entry]["ranking_manual"] = None
+
+        rankings[query_key] = metadata
+    
+    # save rankings
+    with open(f'rankings/{language}.json', 'w') as file:
+        json.dump(rankings, file, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
 
     fetch_rankings()
+    # create_ranking_file("danish")
