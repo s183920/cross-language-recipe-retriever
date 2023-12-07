792a5a28237260c7cbe7290a17cdbe652a4da461
Adam Bosak <bosadam@seznam.cz>
2023-12-07 15:19:53 -0500

    index can save now

--- src/index_creator.py
+++ src/index_creator.py
@@ -11,6 +11,24 @@ def html_to_text(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     return soup.get_text()
 
+def save_json(recipe_text, recipe_name, lang):
+    """
+    Saves the given recipe text as a .json file.
+    """
+
+    # create a dictionary with "docid" and "content" keys
+    doc_data = {"docid": recipe_name[:-5], "content": recipe_text}
+
+    # create the directory if it doesn't exist
+    if not os.path.exists(f"../index/{lang}"):
+        os.makedirs(f"../indexes/json_files/{lang}")
+
+    # save the json file
+    with open(f"../indexes/json_files/{lang}/{recipe_name[:-5]}.json", 'w') as file:
+        json.dump(doc_data, file)
+
+    exit()
+
 def build_index():
     """
     Builds the index for the given recipes and given language.
