4e2c5985bc6de9b6dc0434602d7907708b71c493
Adam Bosak <bosadam@seznam.cz>
2023-12-07 16:22:01 -0500

    indexes work now

--- src/BM25.py
+++ src/BM25.py
@@ -6,17 +6,30 @@ from pyserini.search.lucene import LuceneSearcher
 """
 
 # test english index
-# searcher = LuceneSearcher('../indexes/english_index')
-# hits = searcher.search('chicken egg potato')
+searcher = LuceneSearcher('../indexes/english_index')
+hits = searcher.search('chicken egg potato')
+
+for i in range(len(hits)):
+    print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f}')
+
+
+# test czech index
+# searcher = LuceneSearcher('../indexes/czech_index')
+# searcher.set_language('cs') # set the language
+# hits = searcher.search('kuřecí rýže')
 
 # for i in range(len(hits)):
 #     print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f}')
 
+# test danish index
+# searcher = LuceneSearcher('../indexes/danish_index')
+# searcher.set_language('da') # set the language
+# hits = searcher.search('kylling ris')
 
-# test czech index
-searcher = LuceneSearcher('../indexes/czech_index')
-searcher.set_language('cs') # set the language
-hits = searcher.search('kuřecí rýže')
+# test chinese index
+# searcher = LuceneSearcher('../indexes/chinese_index')
+# searcher.set_language('zh') # set the language
+# hits = searcher.search('鸡 蛋 土豆')
 
 for i in range(len(hits)):
     print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f}')
