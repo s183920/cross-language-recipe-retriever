d23b7a35302a290438f3fcaab62eefbb4243d196
Adam Bosak <bosadam@seznam.cz>
2023-12-07 16:16:58 -0500

    Update BM25.py

--- src/BM25.py
+++ src/BM25.py
@@ -6,16 +6,17 @@ from pyserini.search.lucene import LuceneSearcher
 """
 
 # test english index
-searcher = LuceneSearcher('../indexes/english_index')
-hits = searcher.search('chicken egg potato')
-
-for i in range(len(hits)):
-    print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f}')
-
-
-# test czech index
 # searcher = LuceneSearcher('../indexes/english_index')
 # hits = searcher.search('chicken egg potato')
 
 # for i in range(len(hits)):
 #     print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f}')
+
+
+# test czech index
+searcher = LuceneSearcher('../indexes/czech_index')
+searcher.set_language('cs') # set the language
+hits = searcher.search('kuřecí rýže')
+
+for i in range(len(hits)):
+    print(f'{i+1:2} {hits[i].docid:4} {hits[i].score:.5f}')
