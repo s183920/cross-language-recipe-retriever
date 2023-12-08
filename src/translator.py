aebfb52b346b92f7010ff9b88996889b8e10e006
Lukas Leindals <s183920@student.dtu.dk>
2023-12-08 18:50:30 -0500

    support translatepy

--- src/translator.py
+++ src/translator.py
@@ -3,11 +3,12 @@ This module is responsible for translating the input text
 """
 
 import json
+import time
 
 
 class Translator():
 
-    def __init__(self, languages, dictionary_approach=True, hf_model = "m2m100", verbose = True):
+    def __init__(self, languages, approach="dictionary", hf_model = "m2m100", by_term = False, verbose = True):
         """
             Initialize the translator for the given languages.
             
@@ -16,8 +17,9 @@ class Translator():
 
         self.languages = languages
         self.verbose = verbose
+        self.by_term = by_term
 
-        if dictionary_approach:
+        if approach == "dictionary":
 
             # load the translation dictionary
             with open(f'../translations.json', 'r') as file:
@@ -34,7 +36,7 @@ class Translator():
             # define the translation function
             self.translate = self.translate_dict
 
-        else: # using a translation model
+        elif approach == "hf": # using a translation model
 
             from transformers import pipeline
             
@@ -62,10 +64,43 @@ class Translator():
 
             # define the translation function
             self.translate = self.translate_hf
+            
+        elif approach == "translatepy":
+            from translatepy import Translator as translatepy_Translator
+            self.pytranslator = translatepy_Translator()
+            self.translate = self.translate_translatepy
+            
+        else:
+            raise Exception(f'The approach {approach} is not supported. Use either "dictionary", "translatepy" or "hf".')
+        
+    def translate_translatepy(self, query, language):
+        if language == "english":
+            return query
+        
+        if self.by_term:
+            query = query.split()
+            
+            translation = []
+            for q in query:
+                translation += [self.pytranslator.translate(q, destination_language=language, source_language="english").result]
+                
+            return " ".join(translation)
+        else:
+            return self.pytranslator.translate(query, destination_language=language, source_language="english")
+        
+        
+        
 
     def translate_hf(self, query, language):
+        if self.by_term:
+            query = query.split()
+        
+        # translate the query
         translations = self.hf_translators[language](query)
-        return translations[0]["translation_text"].lower()
+        if self.by_term:
+            return " ".join([translation["translation_text"].lower() for translation in translations])
+        else:
+            return translations[0]["translation_text"].lower()
 
     def translate_dict(self, query, language):
         """
@@ -73,31 +108,142 @@ class Translator():
         """
             
         # return a identity for the english language - query is in english
-        if language == "english":
-            return query
+        # if language == "english":
+        #     return query
         
-        # return the translation from the dictionary
-        else:
+        # # return the translation from the dictionary
+        # else:
             
-            # split the query into words
-            query_words = query.split()
+        # split the query into words
+        query_words = query.split()
 
-            # translate the words
-            translated_words = []
+        # translate the words
+        translated_words = []
 
-            # dictionary translate
-            for word in query_words:
-                if word in self.translation_dict[language].keys():
-                    translated_words.append(self.translation_dict[language][word])
-                else:
-                    translated_words.append(word) # keep the original word if no translation is found
+        # dictionary translate
+        for word in query_words:
+            if word in self.translation_dict[language].keys():
+                translated_words.append(self.translation_dict[language][word])
+            else:
+                translated_words.append(word) # keep the original word if no translation is found
 
-            # join the translated words into a string
-            translated_query = ' '.join(translated_words)
+        # join the translated words into a string
+        translated_query = ' '.join(translated_words)
 
-            return translated_query
+        return translated_query
+        
+def test_translators(verbose = True, by_term = True, acc_by_term = False, hf_translator = "m2m100"):
+    # define languages
+    languages = ["english", "czech", "chinese", "danish"]
+    
+    # initialize the translators
+    t0 = time.time()
+    translator_dict = Translator(languages, approach="dictionary", verbose=verbose, by_term=by_term)
+    print(f"Dictionary translator initialized in {time.time() - t0:.2f} seconds")
+    t0 = time.time()
+    translator_hf = Translator(languages, approach="hf", verbose=verbose, hf_model=hf_translator, by_term=by_term)
+    print(f"HuggingFace translator initialized in {time.time() - t0:.2f} seconds")
+    t0 = time.time()
+    translator_translatepy = Translator(languages, verbose=verbose, approach="translatepy", by_term=by_term)
+    print(f"TranslatePy translator initialized in {time.time() - t0:.2f} seconds")
+    
+    # get the test queries
+    test_queries = [line for line in open("../test_queries.txt", "r").read().split("\n") if line != ""]
+    
+    # initialize counts
+    num_correct_hf = 0
+    num_correct_py = 0
+    num_translations = 0
+    time_dict = 0
+    time_hf = 0
+    time_py = 0
+    
+    # test the translators
+    for language in languages:
+        num_correct_hf_lang = 0
+        num_correct_py_lang = 0
+        num_translations_lang = 0
+        if verbose:
+            print(f"Testing {language}...")
+        for query in test_queries:
+            # translate the query using dictionary approach
+            t0 = time.time()
+            dict_translation = translator_dict.translate(query, language)
+            time_dict += time.time() - t0
+            if verbose:
+                print(f"Dictionary translation took {time.time() - t0:.2f} seconds")
+            
+            # translate the query using hugging face
+            t0 = time.time()
+            hf_translation = translator_hf.translate(query, language)
+            time_hf += time.time() - t0
+            if verbose:
+                print(f"HuggingFace translation took {time.time() - t0:.2f} seconds")
+                
+            # translate the query using translatepy
+            t0 = time.time()
+            py_translation = translator_translatepy.translate(query, language)
+            time_py += time.time() - t0
+            if verbose:
+                print(f"TranslatePy translation took {time.time() - t0:.2f} seconds")
+            
+            
+            # update counts
+            num_translations += 1 if not acc_by_term else len(query.split())
+            num_translations_lang += 1 if not acc_by_term else len(query.split())
+            if not acc_by_term:
+                num_correct_hf += dict_translation == hf_translation
+                num_correct_hf_lang += dict_translation == hf_translation
+                num_correct_py += dict_translation == py_translation
+                num_correct_py_lang += dict_translation == py_translation
+            else:
+                dict_translation_terms = dict_translation.split()
+                hf_translation_terms = hf_translation.split()
+                num_correct_hf += sum([term in hf_translation_terms for term in dict_translation_terms])
+                num_correct_hf_lang += sum([term in hf_translation_terms for term in dict_translation_terms])
+                num_correct_py += sum([term in py_translation for term in dict_translation_terms])
+                num_correct_py_lang += sum([term in py_translation for term in dict_translation_terms])
+            
+            # print results
+            if verbose:
+                print(f"Query: {query}")
+                print(f"Translation (Dictionary): {dict_translation}")
+                print(f"Translation (Hugging Face): {hf_translation}")
+                print(f"Translation (TranslatePy): {py_translation}")
+                print()
+        
+        # calculate accuracy for language
+        print(f"Accuracy for {language} (Hugging Face): {num_correct_hf_lang / num_translations_lang:.2f}\n")
+        print(f"Accuracy for {language} (TranslatePy): {num_correct_py_lang / num_translations_lang:.2f}\n")
+    
+    # calculate results
+    results = {
+        "accuracy_hf" : num_correct_hf / num_translations,
+        "accuracy_py" : num_correct_py / num_translations,
+        "time_dict" : time_dict / num_translations,
+        "time_hf" : time_hf / num_translations,
+        "time_py" : time_py / num_translations,
+    }
+
+    # print results
+    print(f"Accuracy (Hugging Face): {results['accuracy_hf']:.2f}\n")
+    print(f"Accuracy (TranslatePy): {results['accuracy_py']:.2f}\n")
+    print(f"Average time for dictionary translation: {results['time_dict']:.2f} seconds")
+    print(f"Average time for HuggingFace translation: {results['time_hf']:.2f} seconds")
+    print(f"Average time for TranslatePy translation: {results['time_py']:.2f} seconds")    
+    
+    return results
+    
+        
         
 if __name__ == "__main__":
-    translator = Translator(["danish"], dictionary_approach=False, hf_model="m2m100")
-    translation = translator.translate("chicken egg potato", "danish")
-    print(translation)
+    # translator = Translator(["danish", "english"], approach="translatepy", hf_model="m2m100", by_term=True, verbose=True)
+    # translation = translator.translate("chicken egg potato", "english")
+    # print(translation)
+    
+    test_translators(verbose=True, acc_by_term=True, hf_translator="nllb200")
+    
+    # from translatepy import Translator
+    # translator = Translator()
+    # translation = translator.translate("chicken egg potato".split(), "danish")
+    # print(translation)
