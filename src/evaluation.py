439e98f15d2b8b01a0e9c6def0644fbacf6fe407
Lukas Leindals <s183920@student.dtu.dk>
2023-12-10 11:26:26 -0500

    update evaluation code to have AP

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,6 +4,9 @@ import json
 import pandas as pd
 import re
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 LANGUAGES = ["english", "czech", "chinese", "danish"]
 
@@ -18,7 +21,7 @@ class CrossLanguageEvaluator():
     
     def create_labels(self):
         # load rankings
-        
+        rankings = fetch_rankings(LANGUAGES)
 
 
         # create data frame
@@ -52,7 +55,13 @@ class CrossLanguageEvaluator():
             return 0
         
         # get the relevance score
-        relevance_score = ranking[ranking["docid"] == docid]["ranking_manual"].item()
+        relevance_score = ranking[ranking["docid"] == docid]["ranking_manual"]
+        
+        if len(relevance_score) > 1:
+            # raise ValueError(f"More than one relevance score found for query '{query}' and docid '{docid}'")
+            print(f"WARNING: More than one relevance score found for query '{query}' and docid '{docid}'")
+
+        relevance_score = relevance_score.iloc[0]
         
         return relevance_score
     
@@ -66,7 +75,7 @@ class CrossLanguageEvaluator():
         
         return relevance_scores
 
-    def DCG(self, query, p = 10, language = None):
+    def DCGs(self, query, p = 10, language = None):
         """
         Computes DCG@p for a given query
             
@@ -92,7 +101,10 @@ class CrossLanguageEvaluator():
         
         return dcg
     
-    def NDCG(self, query, p = 10, language = None):
+    def DCG(self, query, p = 10, language = None):
+        return self.DCGs(query, p = p, language = language)[p-1]
+    
+    def NDCGs(self, query, p = 10, language = None):
         """
         Computes NDCG@p for a given query
             
@@ -110,19 +122,139 @@ class CrossLanguageEvaluator():
         relevance_scores = self.get_relevance_scores(query, k = p, language = language)
         
         # get dcg
-        dcg = self.DCG(query, p = p, language = language)
+        dcgs = self.DCGs(query, p = p, language = language)
         
         # get ideal dcg
-        ideal_dcg = relevance_scores[np.argsort(relevance_scores)[::-1]].copy().astype(float)
-        ideal_dcg[1:] = ideal_dcg[1:] / np.log2(np.arange(2, len(ideal_dcg) + 1))
-        ideal_dcg = np.cumsum(ideal_dcg)
+        ideal_dcgs = relevance_scores[np.argsort(relevance_scores)[::-1]].copy().astype(float)
+        ideal_dcgs[1:] = ideal_dcgs[1:] / np.log2(np.arange(2, len(ideal_dcgs) + 1))
+        ideal_dcgs = np.cumsum(ideal_dcgs)
         
-        return dcg / ideal_dcg
+        return (dcgs / ideal_dcgs)
     
+    def NDCG(self, query, p = 10, language = None):
+        return self.NDCGs(query, p = p, language = language)[p-1]
+    
+    def AP(self, query, p = 10, language = None):
+        """
+        Computes AP@p for a given query
+            
+        Parameters
+        ---------
+        query:
+            The query for which to compute the AP
+        p:
+            The number of results to consider
+        language:
+            The language for which to compute the AP.    
+            If language is None, the AP is computed for all languages meaning p results are fetched for each language and the AP scores will thus have a length of num languages * p
+        """
+        # get relevance scores
+        relevance_scores = self.get_relevance_scores(query, k = p, language = language)
+                
+        # create document ranks
+        ranks = np.arange(1, len(relevance_scores) + 1)
+        
+        # compute a mask showing the relevant documents
+        relevance_mask = relevance_scores > 0 # ensure binary relevance labels by treating all non-zero labels as relevant
+        
+        # extract the ranks of the relevant documents using the mask
+        relevant_ranks = ranks[relevance_mask]
+        
+        # compute the precision for each relevant document
+        precisions = np.arange(1, len(relevant_ranks)+1) / relevant_ranks
+        
+        return precisions.mean()
+    
+    def evaluate(self, test_queries,p = 10):
+        """
+        Evaluates the cross language retriever on the given test queries.
+        
+        Parameters
+        ---------
+        test_queries:
+            A list of test queries
+        p:
+            The number of results to consider
+        """
+        # create data frame
+        results_df = pd.DataFrame()
+        
+        # evaluate each query
+        for query in test_queries:
+            # get the relevance scores
+            relevance_scores = self.get_relevance_scores(query, k = p)
+            
+            # get the dcg
+            dcg = self.DCG(query, p = p)
+            
+            # get the ndcg
+            ndcg = self.NDCG(query, p = p)
+            
+            # get the ap
+            ap = self.AP(query, p = p)
+            
+            # create data frame
+            df_ = pd.DataFrame({"query": query, "relevance_scores": [relevance_scores], "dcg": dcg, "ndcg": ndcg, "ap": ap})
+            
+            # add to results df
+            results_df = pd.concat([results_df, df_], axis = 0)
+            
+        # reset index
+        results_df = results_df.reset_index(drop = True)
+        
+        return results_df
+    
+
+def plot_evaluation(results, save_path = None):   
+    fig, ax = plt.subplots(figsize = (10, 5))
+    
+    p = len(results["relevance_scores"].iloc[0])   
+    x = np.arange(1, p+1)
+    
+    # ax.scatter(x, results["dcg"], label = "DCG")
+    ax.scatter(x, results["ndcg"], label = f"NDCG@{p}")
+    ax.scatter(x, results["ap"], label = f"AP@{p}")
+    ax.axhline(results["ndcg"].mean(), color = "blue", linestyle = "--", label = f"Mean NDCG@{p}")
+    ax.axhline(results["ap"].mean(), color = "orange", linestyle = "--", label = f"MAP@{p}")
+    
+    # ax.set_xticklabels(results["query"], rotation = 45, ha = "right")
+    ax.set_xticks(x)
+    ax.set_xlabel("Query")
+    ax.set_ylabel("Score")
+    ax.set_title(f"Cross Language Retrieval Evaluation (p = {p})")
+    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+
+    # plt.tight_layout()
+    fig.tight_layout()
+    
+    if save_path is None:
+        plt.show()
+    else:
+        plt.savefig(save_path, bbox_inches='tight')
+        plt.close()
 
 if __name__ == "__main__":
     evaluator = CrossLanguageEvaluator()
     
+    # test on test queries
+    p = 10
+    test_queries = [line.strip() for line in open("../test_queries.txt", "r").readlines()]
+    results = evaluator.evaluate(test_queries, p = p)
+    print(results)
+    print(f"Mean DCG@{p}:", results["dcg"].mean())
+    print(f"Mean NDCG@{p}:", results["ndcg"].mean())
+    print(f"MAP@{p}:", results["ap"].mean())
+    
+    # plot results
+    import os
+    os.makedirs("../plots", exist_ok = True)
+    plot_evaluation(results, save_path = f"../plots/evaluation_{p}.pdf")
+    
+    
+    
     # test query
-    print(evaluator.DCG("sugar spinach seaweed", language="english"))
-    print(evaluator.NDCG("sugar spinach seaweed", language="english"))
+    # print(evaluator.DCG("sugar spinach seaweed"))
+    # print(evaluator.NDCG("sugar spinach seaweed"))
+    # print(evaluator.AP("sugar spinach seaweed"))
+    
+    
